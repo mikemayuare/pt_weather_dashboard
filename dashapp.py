ae390d8bf948d3bbeb69b2d652cfb4c7e8626ea0
Miguelangel Leon Mayuare <93409449+mikemayuare@users.noreply.github.com>
2023-03-11 21:30:57 +0000

    Add files via upload

--- dashapp.py
+++ dashapp.py
@@ -1,16 +1,15 @@
 import pandas as pd
-import numpy as np
 import plotly.express as px
-from dash import Dash, html, dcc, Input, Output, State
+from dash import Dash, html, dcc, Input, Output, ctx
 import dash_bootstrap_components as dbc
 from dash_bootstrap_templates import load_figure_template
 import dash_extensions as de
-from datetime import datetime, date
+from datetime import datetime
 import requests
 
 # set the style
 
-load_figure_template("darkly")
+load_figure_template("solar")
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
 header_font = "https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Cabin:wght@700&family=Lato:wght@900&display=swap"
 
@@ -28,28 +27,84 @@ metrics = [
 observations_url = (
     "https://api.ipma.pt/open-data/observation/meteorology/stations/observations.json"
 )
-obs_r = requests.get(observations_url)
-obs_json = obs_r.json()
-global obs_df
-obs_df = pd.DataFrame()
-obs_df = pd.concat({k: pd.DataFrame(v).T for k, v in obs_json.items()}, axis=0)
-obs_df.reset_index(inplace=True)
-obs_df["level_1"] = obs_df["level_1"].astype("int64")
-obs_df.rename(columns={"level_0": "date", "level_1": "id"}, inplace=True)
-obs_df["humidade"] = obs_df["humidade"].astype("float")
-obs_df = obs_df.merge(stations, on="id")
 
 
+# check if there are missing values (-99) means no data
 def check_nodata(X):
     return [0 if x == -99 else x for x in X]
 
 
-obs_df[metrics] = obs_df[metrics].apply(check_nodata)
+# Set wind directions as recognizable strings
+def get_direction(id):
+    direction_map = {
+        None: "",
+        0: "without direction",
+        1: "N",
+        2: "NE",
+        3: "E",
+        4: "SE",
+        5: "S",
+        6: "SW",
+        7: "W",
+        9: "N",
+    }
+    return direction_map.get(id)
+
+
+# build the dataframe from IPMA API
+def get_stations_metrics():
+    obs_r = requests.get(observations_url)
+    obs_json = obs_r.json()
+    obs_df = pd.DataFrame()
+    obs_df = pd.concat({k: pd.DataFrame(v).T for k, v in obs_json.items()}, axis=0)
+    obs_df.reset_index(inplace=True)
+    obs_df["level_1"] = obs_df["level_1"].astype("int64")
+    obs_df.rename(columns={"level_0": "date", "level_1": "id"}, inplace=True)
+    obs_df["humidade"] = obs_df["humidade"].astype("float")
+    obs_df = obs_df.merge(stations, on="id")
+    obs_df[metrics] = obs_df[metrics].apply(check_nodata)
+    obs_df["idDireccVento"] = obs_df["idDireccVento"].apply(get_direction)
+    obs_df["temperatura"] = obs_df["temperatura"].apply(lambda x: round(x, 0))
+    return obs_df
+
+
+# creates barplot
+def draw_barplot(local, data):
+    df = data[data["local"] == local]
+    fig = px.bar(
+        df,
+        "date",
+        "temperatura",
+        color="humidade",
+        text_auto=True,
+        # color_continuous_scale=["#682D00", "#256AFF"],
+        color_continuous_scale=[
+            (0, "gray"),
+            (0.3, "gray"),
+            (0.3, "green"),
+            (0.6, "green"),
+            (0.6, "#279AF1"),
+            (1, "#279AF1"),
+        ],
+        range_color=[0, 100],
+    )
+
+    fig.update_layout(
+        xaxis_title="Time",
+        yaxis_title="Temperature (C)",
+        coloraxis_colorbar_title="%Rel. Humidity",
+    )
+    return fig
+
+
+obs_df = get_stations_metrics()
+
 
 # images
 termo_hot = "https://assets2.lottiefiles.com/packages/lf20_qkvneqme.json"
 termo_cold = "https://assets5.lottiefiles.com/packages/lf20_vaxtjvo1.json"
-wind = "https://assets7.lottiefiles.com/packages/lf20_qdgvz2hn.json"
+wind = "https://assets7.lottiefiles.com/packages/lf20_khrclx93.json"
+pressure = "https://assets9.lottiefiles.com/packages/lf20_4pku29fg.json"
 options = dict(
     loop=True,
     autoplay=True,
@@ -66,7 +121,6 @@ dropdown = dcc.Dropdown(
 
 # text above dropdown
 dropdown_header = html.P("Choose or type the weather station:")
-hidden = html.Div(hidden="hidden", id="hidden")
 
 # card with temperature info
 card_temp = dbc.Card(
@@ -92,36 +146,21 @@ card_temp = dbc.Card(
     ],
 )
 
-########## place holder for the card below temperature
-card_sensors = dbc.Card(
-    [
-        dbc.CardBody(
-            [
-                html.Ul(
-                    [
-                        html.Li("item 1"),
-                        html.Li("item 2"),
-                        html.Li("item 3"),
-                        html.Li("item 4"),
-                    ]
-                )
-            ]
-        ),
-    ],
-)
 
 # Initialize the dash app
 app = Dash(
     __name__,
-    external_stylesheets=[dbc.themes.DARKLY, dbc_css, header_font],  # CSS
+    external_stylesheets=[dbc.themes.SOLAR, dbc_css, header_font],  # CSS
     meta_tags=[  # for responsiveness
         dict(
             name="viewport",
             content="width=device-width, initial-scale=1.0",
         ),
     ],
+    suppress_callback_exceptions=True,
+    update_title=None,
 )
-
+app.title = "Portugal weather - 24H history"
 
 ########## APP LAYOUT ###########
 app.layout = dbc.Container(
@@ -130,11 +169,11 @@ app.layout = dbc.Container(
             [
                 html.H1(
                     id="header",
-                    children="PORTUGAL WEATHER",
+                    children="PORTUGAL WEATHER 24H HISTORY",
                     style=dict(
                         textAlign="center",
                         fontFamily="Bebas Neue, sans-serif",
-                        backgroundColor="#3D4142",
+                        backgroundColor="#1E434A",
                     ),
                     className="mb-5 pt-2 pb-2",
                 )
@@ -150,65 +189,78 @@ app.layout = dbc.Container(
                         dcc.Graph(id="temp-bar"),
                         dcc.Interval(
                             id="weather_update",
-                            interval=3600 * 500,
+                            interval=600 * 1000,
                             n_intervals=0,
                         ),
-                        hidden,
                     ],
                     width=dict(size=5),
                 ),
             ],
-            align="center",
+            align="end",
         ),
         dbc.Row(
             [
                 dbc.Col(
-                    card_sensors,
+                    id="card-sensors",
                     width=dict(size=3, offset=2),
                 ),
                 dbc.Col(dcc.Graph(id="map-figure"), width=dict(size=5)),
-            ]
+            ],
+            class_name="mt-3",
+        ),
+        dbc.Row(
+            html.Footer(
+                [
+                    "Data extracted from     ",
+                    html.A(
+                        html.Img(
+                            src="https://api.ipma.pt/img/www/ipma.17-vertical-logo.png",
+                            width="60",
+                            height="60",
+                        ),
+                        href="https://api.ipma.pt/",
+                    ),
+                ],
+                style=dict(
+                    backgroundColor="#1E434A",
+                ),
+                className="pt-2 pb-2 ps-5 mt-3",
+            )
         ),
     ],
     fluid=True,
     class_name="dbc",
 )
 
+
 ############# CALLBACKS #############
 
 
-# update dataframe
-@app.callback(Output("hidden", "children"), [Input("weather_update", "n_intervals")])
-def bar_plot_update(n):
-    observations_url = "https://api.ipma.pt/open-data/observation/meteorology/stations/observations.json"
-    obs_r = requests.get(observations_url)
-    obs_json = obs_r.json()
-    global obs_df
-    obs_df = pd.DataFrame()
-    obs_df = pd.concat({k: pd.DataFrame(v).T for k, v in obs_json.items()}, axis=0)
-    obs_df.reset_index(inplace=True)
-    obs_df["level_1"] = obs_df["level_1"].astype("int64")
-    obs_df.rename(columns={"level_0": "date", "level_1": "id"}, inplace=True)
-    obs_df["humidade"] = obs_df["humidade"].astype("float")
-    obs_df = obs_df.merge(stations, on="id")
-    obs_df[metrics] = obs_df[metrics].apply(check_nodata)
-    return None
-
-
 # change barplot location info
-@app.callback(Output("temp-bar", "figure"), [Input("dropdown", "value")])
-def location(value):
-    df = obs_df[obs_df["local"] == value]
-    df["temperatura"] = df["temperatura"].apply(lambda x: round(x, 0))
-    fig = px.bar(
-        df,
-        "date",
-        "temperatura",
-        color="humidade",
-        text_auto=True,
-        color_continuous_scale=["#682D00", "#256AFF"],
-    )
+@app.callback(
+    Output("temp-bar", "figure"),
+    [Input("dropdown", "value"), Input("weather_update", "n_intervals")],
+)
+def update_barplot(value, n):
+    trigger = ctx.triggered_id
+    if trigger == "weather_update":
+        global obs_df
+        obs_df = get_stations_metrics()
+        fig = draw_barplot(value, obs_df)
+
+    else:
+        fig = draw_barplot(value, obs_df)
+
     return fig
+
+
+# # update dataframe
+# @app.callback(Output("temp-bar", "figure"), [Input("weather_update", "n_intervals")])
+# def bar_plot_update(n):
+#     global obs_df
+#     obs_df = get_stations_metrics()
+#     fig = location(dropdown.__getattribute__("value"))
+#     return fig
 
 
 # updates temperature in the dbc card
@@ -235,7 +287,62 @@ def update_temperature(value):
 
 
 # sensors callback
-# @app.callback(Output("map-figure", "figure"), [Input("dropdown", "value")])
+@app.callback(Output("card-sensors", "children"), [Input("dropdown", "value")])
+def update_sensors(value):
+    df = obs_df[obs_df["local"] == value]
+    df_max_date = df[df["date"] == df["date"].max()]
+    card_sensors = dbc.Card(
+        dbc.CardBody(
+            [
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            de.Lottie(
+                                options=options,
+                                width="50%",
+                                height="50%",
+                                url=wind,
+                                id="wind",
+                            )
+                        ),
+                        dbc.Col(
+                            f"{df_max_date['intensidadeVentoKM'].values[0]} km/h {df_max_date['idDireccVento'].values[0]}",
+                            style=dict(fontSize="2rem"),
+                        ),
+                    ],
+                    align="center",
+                ),
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            de.Lottie(
+                                options=options,
+                                width="50%",
+                                height="50%",
+                                url=pressure,
+                                id="pressure",
+                            )
+                        ),
+                        dbc.Col(
+                            f"{df_max_date['pressao'].values[0]} hPa",
+                            style=dict(fontSize="2rem"),
+                        ),
+                    ],
+                    align="center",
+                    class_name="g-0",
+                ),
+                dbc.Row(id="clock", style=dict(fontSize="4rem"), justify="center"),
+                dcc.Interval(id="clock-update", interval=1000, n_intervals=0),
+            ]
+        )
+    )
+    return card_sensors
+
+
+@app.callback(Output("clock", "children"), [Input("clock-update", "n_intervals")])
+def clock_update(n):
+    now = datetime.now()
+    return now.strftime("%H:%M:%S")
 
 
 # map figure update
